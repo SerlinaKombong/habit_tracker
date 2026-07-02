9eee8a8bf66cea5d148c75767005133056901358
kombongserlina-oss <kombongserlina@gmail.com>
2026-07-02 14:34:25 +0800

    Perbaikan halaman ini

---
+++ lib/core/constants.dart
@@ -0,0 +1,4 @@
+class SupabaseConstants {
+  static const String url = 'https://ehfezttmmsitrjharfbd.supabase.co';
+  static const String anonKey = 'sb_publishable_abqHYs5laRluUlopZia6iQ_eKbxgC-8';
+}
